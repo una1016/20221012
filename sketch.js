b4c88fcb4556417645dc022511dadd880db3376a
una1016 <115604573+una1016@users.noreply.github.com>
2022-10-12 11:28:52 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+background(0);
+  noFill()
+  stroke(255)
+
+  for(var i =0;i<int(width/50);i++){
+    for(var j=0;j<int(height/50);j++){
+      stroke("#7EC4CF")
+      ellipse(25+(i*50),25+(j*50),mouseX)
+      stroke(mouseX%256)
+      rectMode(CENTER)
+      rect(25+(i*50),25+(j*50),mouseX)
+      stroke(mouseY%256)
+      ellipse(50+(i*50),50+(j*50),mouseX)
+    }
+  }
+  }
